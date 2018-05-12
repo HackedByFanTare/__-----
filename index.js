8f54e7c2a0b70e05a2eb580f28af042f8e1547cb
ShadowNinners <38014619+ShadowNinners@users.noreply.github.com>
2018-05-12 23:15:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -588,7 +588,7 @@ message.delete();
     .setColor("#320242")
     .setImage('https://media.giphy.com/media/3CX1hiyMyUYgVc8ovc/giphy.gif')
     .setThumbnail(sicon)
-    .setURL('https://discordapp.com/api/oauth2/authorize?client_id=443976303595618305&permissions=8&scope=bot')
+    .setURL('https://discordapp.com/api/oauth2/authorize?client_id=444920637207609354&permissions=8&scope=bot')
     .setFooter("Contacter mon développeur pour savoir des informations precises sur moi",bot.user.displayAvatarURL);
 
   message.author.sendEmbed(help_embed).catch(console.error)
