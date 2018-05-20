caf4ca4109c24865ddc38f59a1319ea7826c09ad
ShadowNinners <38014619+ShadowNinners@users.noreply.github.com>
2018-05-20 18:27:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ bot.on('guildMemberAdd', member => {
     .setColor("#320242")
     .setThumbnail(sicon)
     .setImage('https://media.giphy.com/media/9G59zMKPvyg3ZMniGK/giphy.gif')
-    .setURL('http://discörd.com/6SG8BH')
+    .setURL('https://discord.gg/bteyztq')
     .setFooter("┗(｀Дﾟ┗(｀ﾟДﾟ´)┛ﾟД´)┛┗(｀Дﾟ┗(｀ﾟДﾟ´)┛ﾟД´)┛")
 
   return channel.send(serverembed).catch(console.error);
